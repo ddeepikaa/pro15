b9a71293d3c4df726bfdd53f105eb792fb709536
ddeepikaa <ddeepikaa@users.noreply.github.com>
2016-07-10 11:39:24 +0530

    finish

---
+++ README.c
@@ -0,0 +1,23 @@
+# pro15
+#include<stdio.h>
+int main()
+{
+  int a[]={31,14,16,11,7},ctr;
+  size=sizeof(arr)/sizeof(arr[0]);
+  int temp;
+  for(ctr=0;ctr<size;ctr++)
+  {
+    for(ctr1=ctr+1;ctr1<size;ctr1++)
+    {
+      if(arr[ctr]>arr[ctr1])
+        temp=arr[ctr];
+        arr[ctr]=arr[ctr1];
+        arr[ctr1]=temp;
+    }
+      
+  }
+for(ctr=0;ctr<size;ctr++)
+{
+  printf("%d",arr[0]);
+ }
+}
